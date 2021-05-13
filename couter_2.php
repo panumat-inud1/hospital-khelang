b95d341448a3344bbbac9987cf4fd9d0138e9621
panumat-inud1 <banzwingzaa@hotmail.com>
2021-05-13 10:38:50 +0700

    update 10.30/11/64

---
+++ couter_2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('h.php');
+error_reporting(E_NOTICE);
+?>
+
+<head>
+  <body>
+    <div class="container">
+  <?php include('navbar.php');?>
+  <p></p>
+    <div class="row">
+      <div class="col-md-3">
+     
+        <?php include('menu_left.php');?>
+        
+      </div>
+      <div class="col-md-9"> 
+          
+     
+      <p></p>
+      
+      <?php
+      
+$act = $_GET['act'];
+if($act == 'oder'){
+include('couter_form_oder.php');
+}elseif ($act == 'edit') {
+include('couter_form_edit.php');
+}else {
+include('couter_list_2.php');
+}
+?>
+
+      </div>
+    </div>
+  </div>
+  </body>
+</html>
